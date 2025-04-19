acc412f6d21918e1eaef4549c45ff00b7dee7c1d
Mahmoud00Z <zahabimahmoud@gmail.com>
2025-04-19 23:03:53 +0300

    made a workig registration form

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -16,3 +16,30 @@ register_tab.addEventListener("click", function() {
     register_form.classList.remove("hidden");
     login_form.classList.add("hidden");
 });
+
+register_form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const username = document.getElementById("register_username").value;
+    const password = document.getElementById("register_password").value;
+
+    let users = localStorage.getItem("users");
+    users = users ? JSON.parse(users) : {};
+    if (users[username]) {
+        alert("Username already exists. Please choose a different one.");}
+    else {
+        users[username] = {
+            password: password,
+            scores: []
+        };
+        localStorage.setItem("users", JSON.stringify(users));
+        alert("Registration successful!");
+        register_form.reset();
+        login_tab.click();
+    }
+});
+        
+
+
+
+
+
