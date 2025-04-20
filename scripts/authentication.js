a827c6f3a9f82d0d23435ac327ae8cce14f8f3eb
Mahmoud00Z <zahabimahmoud@gmail.com>
2025-04-20 20:13:09 +0300

    styled the authentication page with css

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -3,18 +3,18 @@ const register_tab = document.getElementById("register_tab");
 const login_form = document.getElementById("login_form");
 const register_form = document.getElementById("register_form");
 
-login_tab.addEventListener("click", function() {
+register_tab.addEventListener("click", function() {
     login_tab.classList.add("active");
     register_tab.classList.remove("active");
-    login_form.classList.remove("hidden");
-    register_form.classList.add("hidden");
+    login_form.classList.remove("active");
+    register_form.classList.add("active");
 });
 
-register_tab.addEventListener("click", function() {
+login_tab.addEventListener("click", function() {
     register_tab.classList.add("active");
     login_tab.classList.remove("active");
-    register_form.classList.remove("hidden");
-    login_form.classList.add("hidden");
+    register_form.classList.remove("active");
+    login_form.classList.add("active");
 });
 
 register_form.addEventListener("submit", function(event) {
