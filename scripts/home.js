b7f931dcaa060c20e00f2c8340bbe8db68f2579f
Mahmoud00Z <zahabimahmoud@gmail.com>
2025-04-20 21:37:27 +0300

    created the rest of the questionnaires linked to local storage and made each quiz into a button

--- scripts/home.js
+++ scripts/home.js
@@ -17,3 +17,89 @@ document.addEventListener("DOMContentLoaded", function () {
                 answer: "COUNT()"
             }
         ],
+        python: [
+            {
+              question: "What is the correct file extension for Python files?",
+              options: [".pt", ".py", ".pyt"],
+              answer: ".py"
+            },
+            {
+              question: "How do you start a comment in Python?",
+              options: ["//", "#", "/*"],
+              answer: "#"
+            },
+            {
+              question: "Which keyword is used to define a function?",
+              options: ["def", "func", "function"],
+              answer: "def"
+            }
+          ],
+          javascript: [
+            {
+              question: "Which keyword is used to declare a variable?",
+              options: ["var", "let", "Both"],
+              answer: "Both"
+            },
+            {
+              question: "How do you write a comment in JavaScript?",
+              options: ["<!-- -->", "//", "##"],
+              answer: "//"
+            },
+            {
+              question: "What is used to parse JSON in JS?",
+              options: ["JSON.parse()", "parse.JSON()", "JSON.convert()"],
+              answer: "JSON.parse()"
+            }
+          ],
+          html: [
+            {
+              question: "What does HTML stand for?",
+              options: ["HyperText Markup Language", "HighText Machine Language", "HyperTool Multi Language"],
+              answer: "HyperText Markup Language"
+            },
+            {
+              question: "Which tag is used to define a paragraph?",
+              options: ["<p>", "<para>", "<text>"],
+              answer: "<p>"
+            },
+            {
+              question: "Which tag is used to insert an image?",
+              options: ["<img>", "<image>", "<pic>"],
+              answer: "<img>"
+            }
+          ],
+          css: [
+            {
+              question: "What does CSS stand for?",
+              options: ["Cascading Style Sheets", "Creative Style System", "Computer Style Sheet"],
+              answer: "Cascading Style Sheets"
+            },
+            {
+              question: "Which property changes text color?",
+              options: ["font-color", "text-color", "color"],
+              answer: "color"
+            },
+            {
+              question: "Which symbol is used for classes in CSS?",
+              options: ["#", ".", "&"],
+              answer: "."
+            }
+          ]
+        };
+      if (!localStorage.getItem("quiz_data")) {
+        localStorage.setItem("quizzes", JSON.stringify (quiz_data));
+      }
+      Object.keys(quizzes).forEach(function (quiz_name) {
+        const card = document.createElement("div");
+        card.className = "quiz_card";
+        card.textContent = quiz_name.toUpperCase();
+
+        card.addEventListener("click", function () {
+            window.location.href = `quiz.html?quiz=${quiz_name}`;
+          });
+      
+          quiz_container.appendChild(card);
+        });
+});
+
+    
